8b056750baef708b6a32efee592263bc438197f8
Giles Knap <giles.knap@diamond.ac.uk>
2024-12-09 14:30:57 +0000

    remove redundant env var

--- settings/config/start-pva.sh
+++ settings/config/start-pva.sh
@@ -3,7 +3,6 @@
 # IP lists for IOCS (blank if get_ioc_ips.py fails)
 export IPS="$(python3 /config/get_ioc_ips.py)"
 export EPICS_PVA_ADDR_LIST=${IPS:-127.0.0.1}
-export EPICS_CA_ADDR_LIST=${IPS:-127.0.0.1}
 
 # PORTS for CA and PVA
 export CA_SERVER_PORT=${CA_SERVER_PORT:-5064}
