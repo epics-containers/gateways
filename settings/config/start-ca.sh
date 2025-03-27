a3d0c1c028d27792442f89924b1a541c45ae33cf
Giles Knap <giles.knap@diamond.ac.uk>
2025-03-27 10:58:55 +0000

    improved settings to go with p47-gateways templates

--- settings/config/start-ca.sh
+++ settings/config/start-ca.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-# IP lists for IOCS (blank if get_ioc_ips.py fails)
-export IPS="$(python3 /config/get_ioc_ips.py)"
-export EPICS_CA_ADDR_LIST=${IPS:-127.0.0.1}
+# EPICS_CA_AUTO_ADDR_LIST=NO implies we are running in cluster network
+#   in this cast we should use the IP list from get_ioc_ips.py
+# EPICS_CA_AUTO_ADDR_LIST=YES implies we are running in hostNetwork
+#   in this case we can broadcast to find local subnet PVs
+
+if [[ ${HOST_NETWORK} == "NO" ]]; then
+  # IP lists for IOCS (blank if get_ioc_ips.py fails)
+  export IPS="$(python3 /config/get_ioc_ips.py)"
+  export EPICS_CA_ADDR_LIST=${IPS:-127.0.0.1}
+fi
+
+if [[ -n ${EPICS_CA_ADDR_LIST} ]]; then
+  cip="-cip ${EPICS_CA_ADDR_LIST}"
+fi
 
 # PORTS for CA and PVA
 export CA_SERVER_PORT=${CA_SERVER_PORT:-5064}
@@ -11,11 +22,6 @@ export PVA_SERVER_PORT=${PVA_SERVER_PORT:-5075}
 # DEBUGGING
 CA_DEBUG=${CA_DEBUG:-0}
 PVA_DEBUG=${PVA_DEBUG:-0}
-
-# don't pass -cip if EPICS_CA_AUTO_ADDR_LIST is YES
-if [[ EPICS_CA_AUTO_ADDR_LIST == "NO" ]]; then
-  cip="-cip ${EPICS_CA_ADDR_LIST}"
-fi
 
 # start the CA Gateway
 /epics/ca-gateway/bin/linux-x86_64/gateway -sport ${CA_SERVER_PORT} $cip \
