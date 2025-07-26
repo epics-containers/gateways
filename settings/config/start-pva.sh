78026c82bbeced2dcb5f294a46eae03a9155c5e7
giles knap <gilesknap@gmail.com>
2025-07-26 07:49:59 +0100

    make helm chart use correct config folder

--- settings/config/start-pva.sh
+++ settings/config/start-pva.sh
@@ -1,41 +1,29 @@
 #!/bin/bash
 
-# EPICS_PVA_AUTO_ADDR_LIST=NO implies we are running in cluster network
-#   in this cast we should use the IP list from get_ioc_ips.py
-# EPICS_PVA_AUTO_ADDR_LIST=YES implies we are running in hostNetwork
-#   in this case we can broadcast to find local subnet PVs
+THIS="$(dirname "$(readlink -f "$0")")"
+source "${THIS}/environment.sh"
 
-THIS=$(realpath $(dirname $0))
-
-# IP lists for IOCS (blank if get_ioc_ips.py fails)
-if [[ ${HOST_NETWORK} == "NO" ]]; then
-  export IPS="$(python3 /config/get_ioc_ips.py)"
-  export EPICS_PVA_ADDR_LIST=${IPS:-127.0.0.1}
+if [[  ${EPICS_PVA_AUTO_ADDR_LIST} = "YES" ]]; then
+  BROADCAST=\"bcastport\":5076,
 else
-  export EPICS_PVA_ADDR_LIST=${EPICS_PVA_ADDR_LIST:-127.0.0.1}
+  BROADCAST=""
 fi
 
-# PORTS for CA and PVA
-export CA_SERVER_PORT=${CA_SERVER_PORT:-5064}
-export PVA_SERVER_PORT=${PVA_SERVER_PORT:-5075}
-
-# DEBUGGING
-CA_DEBUG=${CA_DEBUG:-0}
-PVA_DEBUG=${PVA_DEBUG:-0}
-
-# AUTO_ADDR_LIST
-if [[ ${EPICS_PVA_ADDR_LIST} == "YES" ]]; then
-  export AUTO_ADDR_LIST=true
+if [[ ${EPICS_PVA_AUTO_ADDR_LIST} == "YES" ]]; then
+  auto_addr_list=true
 else
-  export AUTO_ADDR_LIST=false
+  auto_addr_list=false
 fi
 
 # fix up the templated pva gateway config
+set -x
 cat ${THIS}/pvagw.template |
   sed \
-    -e "s/PVA_ADDR_LIST/${EPICS_PVA_ADDR_LIST}/" \
+    -e "s/auto_addr_list/${auto_addr_list}/" \
     -e "s/PVA_SERVER_PORT/${PVA_SERVER_PORT}/" \
-    -e "s/PVA_AUTO_ADDR_LIST/${AUTO_ADDR_LIST}/" \
+    -e "s/EPICS_PVA_AUTO_ADDR_LIST/${EPICS_PVA_AUTO_ADDR_LIST}/" \
+    -e "s/EPICS_PVA_ADDR_LIST/${EPICS_PVA_ADDR_LIST}/" \
+    -e "s/BROADCAST/${BROADCAST}/" \
     > /tmp/pvagw.config
 
 # background the PVA Gateway
